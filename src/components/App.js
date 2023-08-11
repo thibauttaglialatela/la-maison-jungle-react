24939eac452a03894a493e0c555a6c8b3f4d8cdf
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-08-11 15:42:52 +0200

    📦 NEW: add PlantItem component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import '../styles/App.css';
-import Banner from './Banner';
-import Cart from './Cart';
-import ShoppingList from './ShoppingList';
+import Banner from './Banner'
+import logo from '../assets/logo.png'
+// import Cart from './Cart'
+import ShoppingList from './ShoppingList'
 
 function App() {
-  return (
-    <>
-    <Banner />
-    <Cart />
-    <ShoppingList />
-    </>
-  )
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
+			<ShoppingList />
+		</div>
+	)
 }
 
-export default App;
+export default App
