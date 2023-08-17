68a45929b2494a5b02422450a175ecd5056d070f
thibauttaglialatela <thibauttaglialatela@gmail.com>
2023-08-17 14:27:56 +0200

    create state Cart

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-// import Cart from './Cart'
+import Cart from './Cart'
 import ShoppingList from './ShoppingList'
 import Footer from './Footer'
 
@@ -11,8 +11,7 @@ function App() {
 				<img src={logo} alt='La maison jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
-			{/* <Cart /> */}
-
+			<Cart />
 			<ShoppingList />
 			<Footer />
 		</div>
